49f0cef8a4bdb5a18c7e35d359065f0f2432ad4d
AKSHAY <akshaypk408@gmail.com>
2022-04-11 11:10:57 +0530

    test in put

--- src/main/java/baseClasses/BaseClass.java
+++ src/main/java/baseClasses/BaseClass.java
@@ -1,5 +1,13 @@
 package baseClasses;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class BaseClass {
+	@Test
+	 public void login() {
+		Reporter.log("login sucessful",true);
+		 
+	 }
 
 }
